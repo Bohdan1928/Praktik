c9eb50d4ef07320560829bd662923c8d1cace2ed
Бодька <bbigun2000@gmail.com>
2020-11-03 01:08:08 +0200

    Class

--- task1/task1.js
+++ task1/task1.js
@@ -1,10 +1,11 @@
-let element = document.getElementById("main_header");
+/*let element = document.getElementById("main_header");
 element.style.color = "red";
 
 let ul1 = document.getElementsByTagName("ul");
 for(let i = 0; i < ul1.length;i++){
     ul1[i].style.width = "400px";
 }
+
 let width1 = document.getElementsByClassName("linkList");
 for(let i = 0; i <width1.length;i++){
     width1[i].style.width = "50%";
@@ -16,13 +17,53 @@ console.log(text[0].innerText);
 
 let li = document.getElementsByTagName("li");
 for(let i = 0; i < li.length; i++){
-    li[i].style.background = "green";
+    li[i].style.background = "grey";
     console.log(li[i].innerText)
 }
+
 let a = document.getElementsByTagName("a");
 for(let i = 0; i < a.length; i++) {
     a[i].classList.add("anchor");
-    if (i === "link3"){
-        a[i].innerText.style.fontsize = "40px";
+    if (a[i].innerText === "link3"){
+        a[i].style.fontSize = "40px";
     }
 }
+
+/!*let a = document.getElementsByTagName("a");
+for (let i = 0; i < a.length; i++){
+    a[i].classList.add("element_XXX");
+    a[i].innerText = "XXX";*!/
+}*/
+
+/*
+let sub_header = document.getElementsByClassName("sub-header");
+for(let i = 0; i < sub_header.length; i++){
+    let b =  prompt("Color")
+    sub_header[i].style.background = b;
+}*/
+
+/*let sub_header = document.getElementsByClassName("sub-header");
+for(let i = 0; i < sub_header.length; i++){
+    if (sub_header[i].innerHTML === "content 2 segment"){
+        let b =  prompt("Color");
+        sub_header[i].style.background = b;
+    }
+}*/
+
+/*let content_1 = document.getElementsByClassName("content_1");
+for (let i = 0; i < content_1.length; i++){
+    let b =  prompt("Text");
+    content_1[i].innerText = b;
+}*/
+
+/*
+let p = document.getElementsByTagName("p");
+for (let i = 0; i < p.length; i++){
+    p[i].style.padding = "20px";
+}*/
+
+/*
+let text2 = document.getElementsByClassName("text2");
+for (let i = 0; i < text2.length;i++){
+    text2[i].innerText = "xbcjgevojljbj112232";
+}*/
